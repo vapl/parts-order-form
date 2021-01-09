3a381ea3d76864b6ef00ff39c6a41364dcb1a195
Valdis Vascenkovs <valdis.vascenkovs@gmail.com>
2021-01-10 01:38:44 +0200

    adding starting project

---
+++ script.js
@@ -0,0 +1,597 @@
+let materialsDB = [
+
+
+    {
+        thickness: "3 mm",
+        price: 
+        {
+            "A": ["6.33"],
+        },
+        group: 
+        {
+            "A": 
+                [   
+                    "W10250 - White", 
+                    "U5034 - Grey", 
+                    "U156 - Sand Beige", 
+                    "U16024(D1192) - Congo", 
+                    "U12141 virsma Grafit"
+                ]
+        }
+    },
+
+    {
+        thickness: "10 mm",
+        price: 
+        {
+            "A": ["20.14"],
+            "B": ["25.57"],
+            "C": ["36.44"],
+            "G": ["96.14"],
+        },
+        group: 
+        {
+            "A": 
+                [  
+                    "H1334 ST2 Light Sorano Oak", 
+                    "U961 ST9 Graphite Grey", 
+                    "U727 ST9 Stone Grey", 
+                    "U708 ST2 Light Grey", 
+                    "W980 PW Platinum White", 
+                    "K020 ST9 Fireside Select Walnut", 
+                    "U156 ST12 Sand Beige", 
+                    "H3395 ST9 Natural Corbridge Oak", 
+                    "H1733 ST9 Mainau Birch", 
+                    "H3840 - Natural Mandal Maple"
+                ], 
+
+            "B": 
+                [  
+                    "H1334 ST2 Light Sorano Oak", 
+                    "U961 ST9 Graphite Grey", 
+                    "U727 ST9 Stone Grey", 
+                    "U708 ST2 Light Grey", 
+                    "W980 PW Platinum White", 
+                    "K020 ST9 Fireside Select Walnut", 
+                    "U156 ST12 Sand Beige", 
+                    "H3395 ST9 Natural Corbridge Oak", 
+                    "H1733 ST9 Mainau Birch", 
+                    "H3840 - Natural Mandal Maple"
+                ],
+
+            "C": 
+                [  
+                    "H1253 ST19 Truffle Brown Branson Robinia",
+                    "H3192 ST38 Brown Fineline Metallic",
+                    "H1346 ST38 Anthracite Sherman Oak",
+                    "W1000 ST36 Premium White",
+                    "H3154  Dark Brown Charleston Oak",
+                    "H3326  Grey-Beige GladStone Oak"
+                ], 
+        
+            "G": 
+                [  
+                    "Venge Veneer",
+                    "Oak Veneer",
+                    "Painted MDF"
+                ]
+        },
+    },
+
+    {
+        thickness: "16 mm",
+        price: 
+        {
+            "A": ["20.14"],
+            "B": ["25.57"],
+        },
+        group: 
+        {
+            "A": 
+                [   
+                    "U961 ST9 Graphite Grey",
+                    "U708 ST2 Light Grey",
+                    "W980 PW Platinum White"
+                ],
+            "B": 
+                [   
+                    "H3450 ST19 White Fleetwood",
+                    "H3303 ST9 Natural Hamilton Oak",
+                    "F186 ST2 Light Grey Chicago Concrete",
+                    "F501 ST9 Brushed Aluminium",
+                    "U104 ST22 Alabaster White",
+                    "H1424 LN Fineline Cream",
+                    "R20169 ST12 Nabucco",
+                    "H3730  virsma Natural Hickory‌‌"
+                ],
+        }
+    },
+
+    {
+        thickness: "18 mm",
+        price: 
+        {
+            "A": ["20.14"],
+            "B": ["27.74"],
+            "C": ["42.53"],
+            "D": ["63.17"],
+            "E": ["96.30"],
+            "F": ["120.01"],
+            "G": ["153.96"],
+        },
+        group: 
+        {
+            "A": 
+                [
+                    "H1334 ST2 Light Sorano Oak",
+                    "U961 ST9 Graphite Grey",
+                    "U727 ST9 Stone Grey",
+                    "U708 ST2 Light Grey",
+                    "W980 PW Platinum White",
+                    "K020 ST9 Fireside Select Walnut",
+                    "U156 ST12 Sand Beige",
+                    "H3395 ST9 Natural Corbridge Oak",
+                    "H1733 ST9 Mainau Birch",
+                    "H3840 - Natural Mandal Maple"
+                ],
+
+            "B": 
+                [
+                    "U780 ST9 Monument Grey",
+                    "U504 ST9 Tyrolean Blue",
+                    "U626 ST9 Kiwi Green",
+                    "U325 ST22 Antique Rose",
+                    "H3430 ST22 White Aland Pine",
+                    "H3450 ST19 White Fleetwood",
+                    "U201 ST10 Pebble Grey",
+                    "H2033 ST9 Dark Hunton Oak",
+                    "F187 ST10 Dark Grey Chicago Concrete",
+                    "F416 ST16 Beige Textile",
+                    "F637 ST10 White Chromix",
+                    "H1399 ST10 Truffle Brown Denver Oak",
+                    "H3303 ST9 Natural Hamilton Oak",
+                    "U830 ST9 Caramel Nude",
+                    "H1277 ST9 Light Lakeland Acacia",
+                    "U767 ST9 Cubanit Grey",
+                    "U960 ST9 Onyx Grey",
+                    "F186 ST2 Light Grey Chicago Concrete",
+                    "F501 ST9 Brushed Aluminium",
+                    "U732 ST9 Dust Grey",
+                    "U702 ST9 Cashmere Grey",
+                    "U748 ST9 Truffle Brown",
+                    "W1000 ST9 Premium White",
+                    "U104 ST22 Alabaster White",
+                    "H1424 LN Fineline Cream",
+                    "R20169 ST12 Nabucco",
+                    "H1199 ST10 Black-Brown Thermo Oak‌‌",
+                    "H3730  virsma Natural Hickory‌‌"
+                    ],
+
+            "C": 
+                [
+                    "H1253 ST19 Truffle Brown Branson Robinia",
+                    "H3192 ST38 Brown Fineline Metallic",
+                    "U998 ST32 Shadow Black",
+                    "H1346 ST38 Anthracite Sherman Oak",
+                    "W1000 ST36 Premium White",
+                    "H3154  Dark Brown Charleston Oak",
+                    "H3326  Grey-Beige GladStone Oak",
+                    "LK90 virsma Dark Oak 'magister tabacco'"
+                ],
+
+            "D": 
+                [
+                    "FA33 virsma Aged Bronze, textile"
+                ],
+
+            "E": 
+                [
+                    "1919L gloss Cemento",
+                    "6340B gloss Bigio Metallic",
+                    "1678L gloss Rame Metallic",
+                    "5026B gloss Meringa"
+                ],
+
+            "F": 
+                [
+                    "M018 gloss Blu Vetro",
+                    "M055 gloss Cammello",
+                    "M021 virsma Tortora"
+                ],
+
+            "G": 
+                [
+                    "Venge Veneer",
+                    "Oak Veneer",
+                    "Painted MDF"
+                ],
+        }
+    },
+
+    {
+        thickness: "19 mm",
+        price: 
+        {
+            "G": ["153.96"],
+        },
+        group: 
+        {
+            "G": 
+                [   
+                    "AL06 metals Brushed Bronze",
+                    "AL04 metals Brushed Gold",
+                    "AL03  Brushed Inox",
+                    "S2-PA2S01#19  Natural Oak (Veneer)",
+                    "S2-PA2S06#19  Mystery Oak (Veneer)",
+                    "S2-PA2S04#19  Desert Oak (Veneer)",
+                    "S2-PA2S15#19  Smoked Walnut (Veneer)"
+                ],
+        }
+    },
+
+    {
+        thickness: "20 mm",
+        price: 
+        {
+            "G": ["153.96"],
+        },
+        group: 
+        {
+            "G": 
+                [   
+                    "0032 matt Fenix Bianco Kos",
+                    "0725 matt Fenix Grigio Efeso",
+                    "0720 metals Fenix Nero Ingo"
+                ],
+        }
+    },
+
+    {
+        thickness: "25 mm",
+        price: 
+        {
+            "A": ["28.65"],
+            "B": ["34.32"],
+            "C": ["45.66"],
+            "G": ["119.24"],
+        },
+        group: 
+        {
+            "A": 
+                [   
+                    "H1334 ST2 Light Sorano Oak",
+                    "U961 ST9 Graphite Grey",
+                    "U727 ST9 Stone Grey",
+                    "U708 ST2 Light Grey",
+                    "W980 PW Platinum White",
+                    "K020 ST9 Fireside Select Walnut",
+                    "U156 ST12 Sand Beige",
+                    "H3395 ST9 Natural Corbridge Oak",
+                    "H1733 ST9 Mainau Birch",
+                    "H3840 - Natural Mandal Maple"
+                ],
+
+            "B": 
+                [   
+                    "H3430 ST22 White Aland Pine",
+                    "H3450 ST19 White Fleetwood",
+                    "F416 ST16 Beige Textile",
+                    "H1399 ST10 Truffle Brown Denver Oak",
+                    "H3303 ST9 Natural Hamilton Oak",
+                    "H1277 ST9 Light Lakeland Acacia",
+                    "U767 ST9 Cubanit Grey",
+                    "U960 ST9 Onyx Grey",
+                    "F186 ST2 Light Grey Chicago Concrete",
+                    "F501 ST9 Brushed Aluminium",
+                    "U702 ST9 Cashmere Grey",
+                    "U748 ST9 Truffle Brown",
+                    "W1000 ST9 Premium White",
+                    "U104 ST22 Alabaster White",
+                    "H1424 LN Fineline Cream",
+                    "R20169 ST12 Nabucco",
+                    "H1199 ST10 Black-Brown Thermo Oak‌‌",
+                    "H3730  virsma Natural Hickory‌‌"
+                ],
+
+            "C": 
+                [   
+                    "H1253 ST19 Truffle Brown Branson Robinia",
+                    "H3192 ST38 Brown Fineline Metallic",
+                    "U998 ST32 Shadow Black",
+                    "H1346 ST38 Anthracite Sherman Oak",
+                    "W1000 ST36 Premium White",
+                    "H3154  Dark Brown Charleston Oak",
+                    "H3326  Grey-Beige GladStone Oak"
+                ],
+
+            "G": 
+                [
+                    "Venge Veneer",
+                    "Oak Veneer",
+                    "Painted MDF"
+                ],
+        }
+    },
+    {
+        thickness: "38 mm",
+        price: 
+        {
+            "A": ["42.79"],
+            "B": ["42.79"],
+            "C": ["52.62"],
+            "G": ["150.74"],
+        },
+        group: 
+        {
+            "A": 
+                [   
+                    "H1334 ST2 Light Sorano Oak"
+                ],
+
+            "B": 
+                [   
+                    "U767 ST9 Cubanit Grey",
+                    "W1000 ST9 Premium White",
+                    "H1424 LN Fineline Cream",
+                    "H3730  virsma Natural Hickory‌‌"
+                ],
+
+            "C": 
+                [   
+                    "H3192 ST38 Brown Fineline Metallic",
+                    "W1000 ST36 Premium White",
+                    "H3154  Dark Brown Charleston Oak"
+                ],
+            "G": 
+                [
+                    "Venge Veneer",
+                    "Oak Veneer",
+                    "Painted MDF"
+                ],
+        }
+    }
+]
+
+
+let selectThk = document.getElementById('thk');
+let selectGroup = document.getElementById('group');
+let selectMatName = document.getElementById('matName');
+
+// Get a materials thickness
+const thkMat = materialsDB.map(thk => {
+    let {thickness} = thk;
+    let matThickness = document.createElement('option');
+    matThickness.innerText = thickness;
+    matThickness.setAttribute('value', thickness);
+    selectThk.appendChild(matThickness);
+});
+
+const filterArray = () => {
+    return materialsDB.filter(r => r.thickness === selectThk.value);
+};
+
+
+const groupValues = () => {
+    
+    selectGroup.innerText = ''; // Remove options by changing thickness
+
+    // Get the materials price group keys and append them to selectGroup
+    Object.keys(filterArray()[0].group).forEach((key) => {
+        let groupOption = document.createElement('option');
+        groupOption.innerText = key;
+        groupOption.setAttribute('value', key);
+        selectGroup.appendChild(groupOption);
+    });
+    materialValues();
+};
+
+const materialValues = () => {
+
+    selectMatName.innerText = '';
+
+    //selectMatName.innerText = '';
+    Object.values(filterArray()[0].group[selectGroup.value]).forEach((value) => {
+        let matOption = document.createElement('option');
+        matOption.innerText = value;
+        matOption.setAttribute('value', value);
+        selectMatName.appendChild(matOption);
+    });
+}
+
+// Event listener for materials thickness change
+selectThk.addEventListener('change', groupValues);
+selectGroup.addEventListener('change', materialValues);
+
+
+
+
+/****** m2 Calculation *******/
+
+const lengthInput = document.getElementById('length');
+const widthInput = document.getElementById('width');
+const quantityInput = document.getElementById('quantity');
+
+const grooveValue = document.getElementById('grooveValue');
+const millingValue = document.getElementById('millingValue');
+const figuralProcessingValue = document.getElementById('figuralProcessingValue');
+const drillingValue = document.getElementById('drillingValue');
+
+const m2 = document.getElementById('m2');
+m2.disabled = true;
+
+const enterValue = () => {
+
+    let sqMeters = ((lengthInput.value / 1000) * (widthInput.value / 1000)) * quantityInput.value;
+
+    m2.value = sqMeters.toFixed(2);
+
+    if (isNaN(lengthInput.value) || isNaN(widthInput.value) || isNaN(quantityInput.value)) {
+
+        alert('Atļauts ievadīt tikai skaitļus');
+
+        lengthInput.value = '';
+        widthInput.value = '';
+        quantityInput.value = '';
+
+        m2.value = '0.00';
+    };
+    if (isNaN(grooveValue.value) || isNaN(drillingValue.value) || isNaN(millingValue.value) || isNaN(figuralProcessingValue.value)) {
+
+        alert('Atļauts ievadīt tikai skaitļus');
+
+        grooveValue.value = '';
+        drillingValue.value = '';
+        millingValue.value = '';
+        figuralProcessingValue.value = '';
+    };
+};
+
+lengthInput.addEventListener('input', enterValue);
+widthInput.addEventListener('input', enterValue);
+quantityInput.addEventListener('input', enterValue);
+grooveValue.addEventListener('input', enterValue);
+drillingValue.addEventListener('input', enterValue);
+millingValue.addEventListener('input', enterValue);
+figuralProcessingValue.addEventListener('input', enterValue);
+
+/****** / m2 Calculation *******/
+
+/****** Adding items to table *******/
+
+const tbody = document.getElementById('tableTbody');
+const tableHead = document.getElementById('tableHead');
+const consecutiveNumber = document.getElementById('itemNumber');
+const btnAddItem = document.getElementById('addItem');
+const abs1 = document.getElementById('abs1');
+const abs2 = document.getElementById('abs2');
+const groove = document.getElementById('groove');
+const drilling = document.getElementById('drilling');
+const milling = document.getElementById('milling');
+const figuralProcessing = document.getElementById('figuralProcessing');
+
+
+
+
+/* Disable or enable prcessing value input and checkbox boolian */
+grooveValue.disabled = true;
+drillingValue.disabled = true;
+millingValue.disabled = true;
+figuralProcessingValue.disabled = true;
+
+const toggleCheckboxGroove = () => {
+    grooveValue.toggleAttribute('disabled');
+    grooveValue.value = '';
+}
+const toggleCheckboxDrilling = () => {
+    drillingValue.toggleAttribute('disabled');
+    drillingValue.value = '';
+}
+const toggleCheckboxMill = () => {
+    millingValue.toggleAttribute('disabled');
+    millingValue.value = '';
+}
+const toggleCheckboxFiguralProcessing = () => {
+    figuralProcessingValue.toggleAttribute('disabled');
+    figuralProcessingValue.value = '';
+}
+/* / Disable or enable prcessing value input and checkbox boolian */
+
+
+groove.addEventListener('click', toggleCheckboxGroove);
+drilling.addEventListener('click', toggleCheckboxDrilling);
+milling.addEventListener('click', toggleCheckboxMill);
+figuralProcessing.addEventListener('click', toggleCheckboxFiguralProcessing);
+
+
+const createItems = () => {
+
+    /* alerts if Processing values are empty */
+    if (!grooveValue.disabled && grooveValue.value === '') {
+            
+        alert('Ievadi gropes garumu!');
+        return true;
+    };
+    if (!drillingValue.disabled && drillingValue.value === '') {
+
+        alert('Ievadi urbumu skaitu!');
+        return true;
+    };
+    if (!millingValue.disabled && millingValue.value === '') {
+
+        alert('Ievadi frēzēšanas garumu!');
+        return true;
+    };
+    if (!figuralProcessingValue.disabled && figuralProcessingValue.value === '') {
+
+        alert('Ievadi figurālās detaļas apstrādes garumu');
+        return true;
+    };
+    /* / alerts if Processing values are empty */
+
+
+
+    let row = tbody.insertRow(0);
+    row.setAttribute('id', 'item');
+
+    let consecutiveNumber = document.createElement('th');
+    row.prepend(consecutiveNumber);
+
+    let cellItemThickness = row.insertCell();
+    let cellItemPriceGroup = row.insertCell();
+    let cellItemMaterial = row.insertCell();
+    let cellItemLength = row.insertCell();
+    let cellItemAbs1 = row.insertCell();
+    let cellItemWidth = row.insertCell();
+    let cellItemAbs2 = row.insertCell();
+    let cellItemQuantity = row.insertCell();
+    let cellItemM2 = row.insertCell();
+    let cellItemGrp = row.insertCell();
+    let cellItemUrb = row.insertCell();
+    let cellItemFr = row.insertCell();
+    let cellItemFigDet = row.insertCell();
+    let cellItemPrice = row.insertCell();
+    
+
+    consecutiveNumber.innerText = 1;
+    cellItemThickness.innerText = selectThk.value;
+    cellItemPriceGroup.innerText = selectGroup.value;
+    cellItemMaterial.innerText = selectMatName.value;
+    cellItemLength.innerText = lengthInput.value;
+    cellItemAbs1.innerText = abs1.value;
+    cellItemWidth.innerText = widthInput.value;
+    cellItemAbs2.innerText = abs2.value;
+    cellItemQuantity.innerText = quantityInput.value;
+    cellItemM2.innerText = m2.value;
+    if (grooveValue.value.length > 0) {
+        cellItemGrp.innerText = grooveValue.value + 'mm';        
+    } else {
+        cellItemGrp.innerText = '-';
+    };
+    if (drillingValue.value.length > 0) {
+        cellItemUrb.innerText = drillingValue.value + 'gb';        
+    } else {
+        cellItemUrb.innerText = '-';
+    };
+    if (millingValue.value.length > 0) {
+        cellItemFr.innerText = millingValue.value + 'mm';
+    } else {
+        cellItemFr.innerText = '-';
+    };
+    if (figuralProcessingValue.value.length > 0) {
+        cellItemFigDet.innerText = figuralProcessingValue.value + 'mm';
+    } else {
+        cellItemFigDet.innerText = '-';
+    };
+
+    Object.values(filterArray()[0].price[selectGroup.value]).forEach((itemPrice) => {
+        let priceCalc = itemPrice * m2.value;
+        cellItemPrice.innerText = priceCalc.toFixed(2) + '€';
+    });
+
+
+
+    
+}
+
+btnAddItem.addEventListener('click', createItems);
+
